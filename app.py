86b1ec4fbf4db7ad0e716330f3fb8ba280074008
Stanley Yoshinori Takamatsu <stanleytakamatsu@gmail.com>
2017-06-03 20:02:26 -0300

    Change hello return value

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return 'Hello Yros!'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
